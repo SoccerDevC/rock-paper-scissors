4b8f6aa33a702eb3f7f92e913778de15ca88e99c
SoccerDevC <cddavid2001@gmail.com>
2024-02-12 11:33:17 +0300

    function that generates the computer choice

---
+++ script.js
@@ -0,0 +1,7 @@
+const computerChoices =["Rock", "Paper","Scissors"];
+
+function getComputerChoice(){
+    let randomChoice = Math.floor(Math.random()*3);
+    let computerChoice = computerChoices[randomChoice];
+    
+}
